temporarily removed gradient accumulation; fixed typos

objective.py
@@ -1,5 +1,5 @@
 import torch
-import torch.functional as F
+import torch.nn.functional as F
 from torch import nn
 
 
@@ -13,7 +13,7 @@ def contrastive_loss(x_batch1,
     Args:
       x_batch* (tensor): minibatches of augmented samples of shape
                         (batch_size, out_dim).
-      temperature (float): for teperature scaling.
+      temperature (float): for temperature scaling.
       normalize (boolean):  indicate whether we normalize the input to the loss
                             function.
       weight (tensor): weights for loss of each sample in the minibatch.

pretrain.py
@@ -1,27 +1,29 @@
 from data import get_augmented_dataloader
 from models.simclr import SimCLRMain
-from utils.model_utils import train_simclr
+from utils.model_utils import train_simclr, train_simclr_no_accum
 import torch
-import matplotlib.pyplot as plt
-import numpy as np
 import json
+from argparse import ArgumentParser
 
 
-# need to permute the numpy image in order to display it correctly
-def show(img):
-    npimg = img.cpu().numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-
-# Denormalize images for visualization
-def denorm(x, channels=None, w=None, h=None, resize=False):
-    x = 0.5 * (x + 1)
-    x = x.clamp(0, 1)
-    if resize:
-        if channels is None or w is None or h is None:
-            print('Number of channels, width and height must be provided for resize.')
-        x = x.view(x.size(0), channels, w, h)
-    return x
+def create_parser(configs):
+    parser = ArgumentParser()
+    parser.add_argument('--n_epoch',
+                        default=configs['n_epoch'],
+                        type=int,
+                        help='number of epochs to train')
+    parser.add_argument('--accum_steps',
+                        default=configs['accum_steps'],
+                        type=int,
+                        help='number of gradient accumulation steps; total batch size is 64*accum_steps')
+    parser.add_argument('--save_every',
+                        default=configs['save_ckpt_every'],
+                        type=int,
+                        help='frequency for saving checkpoint during training')
+    parser.add_argument('--batch_size',
+                        default=configs['default_batch_size'],
+                        type=int)
+    return parser
 
 
 if __name__ == '__main__':
@@ -36,26 +38,24 @@ if __name__ == '__main__':
     with open('utils/configs.json') as f:
         configs = json.load(f)
 
+    parser = create_parser(configs)
+    args = parser.parse_args()
+
     # Get data
     loader_train_simclr = get_augmented_dataloader(batch_size=configs['batch_size_small'],
                                                    train_mode='pretrain')
-    loader_train_clf, loader_eval_clf = get_augmented_dataloader(
-        batch_size=configs['batch_size'],
-        train_mode='lin_eval'
-    )
-    loader_test_clf = get_augmented_dataloader(batch_size=configs['batch_size'],
-                                               train_mode='test')
 
     simclr_model = SimCLRMain()
     base_optim = torch.optim.Adam(simclr_model.parameters(), lr=configs['lr'], weight_decay=configs['wt_decay'])
-    train_simclr(model=simclr_model,
-                 optimizer=base_optim,
-                 loader_train=loader_train_simclr,
-                 device=device,
-                 n_epochs=configs['n_epoch'],
-                 accum_steps=configs['accum_steps'],
-                 temperature=configs['temp']
-                 )
+    train_simclr_no_accum(model=simclr_model,
+                          optimizer=base_optim,
+                          loader_train=loader_train_simclr,
+                          device=device,
+                          n_epochs=args.n_epoch,
+                          save_every=args.save_every,
+                          batch_size=args.batch_size,
+                          temperature=configs['temp']
+                          )
     # TODO: Create a flexible training procedure, so we can choose among ['pretrain', 'lin_eval', 'fine_tune'] using
     #  args with one training file features_train, targets_train = feature_extraction( simclr_model=simclr_model,
     #  device=device, loader_lin_eval=loader_train_clf)

utils/model_utils.py
@@ -15,6 +15,8 @@ def train_simclr(model,
                  device,
                  accum_steps,
                  temperature,
+                 save_every,
+                 save_ckpt=True,
                  checkpt_path=None):
     """
     Pretrain a SimCLR model with ResNet50 as the encoder.
@@ -25,8 +27,90 @@ def train_simclr(model,
     loader_train (DataLoader): dataloader containing training data.
     temperature (float): temperature used in NT-XENT.
     epochs (int): the number of epochs to train for.
-    accum_steps (int): number of steps to accumulate gradient for.
     device (torch.device): 'cuda' or 'cpu' depending on the availability of GPU.
+    accum_steps (int): number of steps to accumulate gradients.
+    save_every (int): frequency for saving the model.
+    save_ckpt (bool): indicate whether to save checkpoints.
+    checkpt_path (str): if resuming training from a checkpoint, provide the path.
+
+    Returns: Nothing.
+    """
+    if checkpt_path is not None:
+        checkpoint = torch.load(checkpt_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        current_epoch = checkpoint['epoch']
+    else:
+        current_epoch = 0
+
+    total_batch_size = int(configs["batch_size_small"] * accum_steps)
+
+    # For saving the model
+    sample_inputs, _, _ = next(iter(loader_train))
+    fixed_input = sample_inputs[:32, :, :, :]
+
+    optimizer.zero_grad()
+    print_every = 100
+    model = model.to(device=device)
+    for e in range(current_epoch, n_epochs):
+        for t, (x1, x2, _) in enumerate(loader_train):
+            model.train()
+            x1 = x1.to(device=device, dtype=torch.float32)
+            x2 = x2.to(device=device, dtype=torch.float32)
+            _, z1 = model(x1)
+            _, z2 = model(x2)
+            loss = contrastive_loss(z1, z2, temperature) / accum_steps
+            # Gradient accumulation
+            loss.backward()
+            if (t + 1) % accum_steps == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+
+            if t % print_every == 0:
+                print('Epoch: %d, Iteration %d, loss = %.4f' % (e, t, loss.item()))
+        if save_ckpt:
+            if (e + 1) % save_every == 0:
+                torch.save({
+                    'epoch': e,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss.item(),
+                }, "/vol/bitbucket/ss9920/checkpoints/simclr_ckpt_bs{}_nepoch{}.pth".format(
+                    total_batch_size,
+                    (e + 1)))
+        else:
+            pass
+    # save the model
+    model.eval()
+    with torch.no_grad():
+        torch.jit.save(torch.jit.trace(model, fixed_input.to(device), check_trace=False),
+                       "/vol/bitbucket/ss9920/project-results/simclr_model_bs{}_nepoch{}.pth".format(
+                           total_batch_size,
+                           n_epochs))
+
+
+def train_simclr_no_accum(model,
+                          optimizer,
+                          loader_train,
+                          n_epochs,
+                          device,
+                          temperature,
+                          save_every,
+                          batch_size,
+                          save_ckpt=True,
+                          checkpt_path=None):
+    """
+    Pretrain a SimCLR model with ResNet50 as the encoder.
+
+    Args:
+    model: A SimCLRMain model.
+    optimizer: An Optimizer object we will use to train the model.
+    loader_train (DataLoader): dataloader containing training data.
+    temperature (float): temperature used in NT-XENT.
+    epochs (int): the number of epochs to train for.
+    device (torch.device): 'cuda' or 'cpu' depending on the availability of GPU.
+    save_every (int): frequency for saving the model.
+    save_ckpt (bool): indicate whether to save checkpoints.
     checkpt_path (str): if resuming training from a checkpoint, provide the path.
 
     Returns: Nothing.
@@ -45,7 +129,6 @@ def train_simclr(model,
 
     optimizer.zero_grad()
     print_every = 100
-    # save_every = 20  # save model every x epochs
     model = model.to(device=device)
     for e in range(current_epoch, n_epochs):
         for t, (x1, x2, _) in enumerate(loader_train):
@@ -54,31 +137,32 @@ def train_simclr(model,
             x2 = x2.to(device=device, dtype=torch.float32)
             _, z1 = model(x1)
             _, z2 = model(x2)
-            loss = contrastive_loss(z1.cpu(), z2.cpu(), temperature)
-            # Gradient accumulation
+            loss = contrastive_loss(z1, z2, temperature)
+
             loss.backward()
-            if (t + 1) % accum_steps == 0:
-                optimizer.step()
-                optimizer.zero_grad()
+            optimizer.step()
+            optimizer.zero_grad()
 
             if t % print_every == 0:
                 print('Epoch: %d, Iteration %d, loss = %.4f' % (e, t, loss.item()))
-
-        if (e + 1) % configs["save_checkpt_every"] == 0:
-            torch.save({
-                'epoch': e,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'loss': loss.item(),
-            }, "/vol/bitbucket/ss9920/checkpoints/simclr_ckpt_bs{}_nepoch{}.pth".format(
-                configs["batch_size_small"] * configs["accum_steps"],
-                (e + 1)))
+        if save_ckpt:
+            if (e + 1) % save_every == 0:
+                torch.save({
+                    'epoch': e,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss.item(),
+                }, "/vol/bitbucket/ss9920/checkpoints/simclr_ckpt_bs{}_nepoch{}.pth".format(
+                    batch_size,
+                    (e + 1)))
+        else:
+            pass
     # save the model
     model.eval()
     with torch.no_grad():
         torch.jit.save(torch.jit.trace(model, fixed_input.to(device), check_trace=False),
                        "/vol/bitbucket/ss9920/project-results/simclr_model_bs{}_nepoch{}.pth".format(
-                           configs["batch_size_small"] * configs["accum_steps"],
+                           batch_size,
                            n_epochs))
 
 